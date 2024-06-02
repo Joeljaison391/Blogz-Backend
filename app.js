bf32f894709167d86cf85bb0061576f5b9a0d116
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-06-02 14:12:03 +0530

    Refactor app.js to use CommonJS module syntax for compatibility

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import path from "path";
+const express = require('express');
+const path = require('path');
 
 
 const app = express();
@@ -10,4 +10,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-export default app;
+module.exports = app;
