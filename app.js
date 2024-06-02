fe6ac7a82a5ef3ecb3828b1a59d834a7768f9c66
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-06-02 14:34:55 +0530

    chore: Enable CORS for specified origin URLs in app.js

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const express = require('express');
 const path = require('path');
 const prisma = require('./config/prismaDb');
 
+const cors = require('cors');
+
 const app = express();
+
+const ORIGIN_URLS = process.env.ORIGIN_URLS ? process.env.ORIGIN_URLS.split(',') : [];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        // Check if the origin is in the allowed list or if there's no origin (like for same-origin requests)
+        if (ORIGIN_URLS.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
