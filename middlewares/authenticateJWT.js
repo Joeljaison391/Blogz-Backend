4062053daac36945e0b6d6159a605a8463de394c
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-06-07 08:27:14 +0530

    feat: Add middleware for JWT authentication

---
+++ middlewares/authenticateJWT.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateJWT = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication token not found' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid authentication token' });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateJWT;
---
+++ utils/jwtUtils.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (user) => {
+  return jwt.sign(
+    { userId: user.id, username: user.username, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' }
+  );
+};
+
+module.exports = { generateToken };
