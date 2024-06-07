8da807cf8f63c84d346616626af01730afd8d8c3
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-06-07 20:10:25 +0530

    feat: Add PasswordResetToken and EmailVerificationToken models and migrations

---
+++ prisma/migrations/20240607143704_add_email_verification_token_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "EmailVerificationToken" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "EmailVerificationToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "EmailVerificationToken_token_key" ON "EmailVerificationToken"("token");
+
+-- AddForeignKey
+ALTER TABLE "EmailVerificationToken" ADD CONSTRAINT "EmailVerificationToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
